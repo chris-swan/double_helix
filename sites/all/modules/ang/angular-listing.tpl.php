15d6d686ff49e5ebfd2c07b6e1835016bd135f0a
chris-swan <chris@chrisswan.com>
2015-11-13 13:55:30 -0800

    fix type-o in js files.

--- sites/all/modules/ang/angular-listing.tpl.php
+++ sites/all/modules/ang/angular-listing.tpl.php
@@ -1,19 +1,19 @@
 <div ng-app="nodeListing">
 
-	<div ng-controller="ListController">
+   <div ng-controller="ListController">
 
-		<h3>Filter</h3>
-		<input ng-model="search" ng-change="doSearch()">
+     <h3>Filter</h3>
+     <input ng-model="search" ng-change="doSearch()">
 
-		<ul>
-			<li ng-repeat="node in nodes"><button ng-click="open(node.nid)">Open</button> {{ node.title }}</li>
-		</ul>
+      <ul>
+        <li ng-repeat="node in nodes"><button ng-click="open(node.nid)">Open</button> {{ node.title }}</li>
+      </ul>
 
-		<script type="text/ng-template" id="loadedNodeTemplate">
-		<h3>{{ loadedNode.title }}</h3>
-		{{ loadedNode.body }}
-		</script>
+     <script type="text/ng-template" id="loadedNodeTemplate">
+     <h3>{{ loadedNode.title }}</h3>
+     {{ loadedNode.body }}
+     </script>
 
-	</div>
+    </div>
 
 </div>
